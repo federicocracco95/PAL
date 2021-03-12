9c7b7466f71ee01a2326b52b4fb69ce09dd7cb10
francescosalzillo <fra230799@gmail.com>
2021-03-12 12:55:43 +0100

    add element in modifica profailo

--- client/src/views/Modifica-profilo.vue
+++ client/src/views/Modifica-profilo.vue
@@ -1,13 +1,41 @@
 <template>
     <div>
-        <div class="flex items-stretch ...">
-            <div class="py-4 box-border h-32 w-32 p-4 border-4">1</div>
-            <div class="py-12 box-border h-32 w-32 p-4 border-4">2</div>
-            <h1> HELLOHELLO</h1>
+        <link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
+        crossorigin="anonymous">
+        <div class="flex flex-row items-stretch   ">
+            <div class="py-4 box-border h-1/2 w-1/2  p-4 ">
+            <p class="text-center font-bold text-blue-700">MODIFICA PROFILO </p>
+             <div class="self-center ">
+                   <i class="fas fa-user-circle h-30"></i>
+             </div>
+            </div>
+
+
+            <div class="py-4 box-border h-1/2 w-1/2 p-4 ">
+                <div class=" md:gap-2"> 
+                <input type="text" name="mail" class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm" placeholder="Nome"> 
+                <input type="text" name="mail" class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm" placeholder="Cognome">
+                <div class="flex flex-row ">
+                    <input type="text" name="mail" class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm " placeholder="nome.cognome@gmail.com  "> <i class="fas fa-pencil-alt self-center pl-2"></i>
+                </div>
+                <div class="flex flex-row ">
+                <input type="text" name="mail" class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm" placeholder="Password"> <i class="fas fa-pencil-alt self-center pl-2"></i>
+                </div>
+
+                </div>
+            </div>
+            
+       
+           
         </div>
+         <div class="object-center"> 
+             <button class="bg-blue-800 outline-none text-white font-bold py-2 px-4 rounded pl-1/2"><span>Salva modifiche</span></button>
+         </div>
+             
+                    
 
-
-        
     </div>
 </template>
 
