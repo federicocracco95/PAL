9f3363398849fdd487f0658c2f8bd057c6d109f7
francescosalzillo <fra230799@gmail.com>
2021-03-15 11:36:54 +0100

    finish modifica-profilo

--- client/src/views/Modifica-profilo.vue
+++ client/src/views/Modifica-profilo.vue
@@ -4,16 +4,18 @@
         href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
         integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
         crossorigin="anonymous">
-        <div class="flex flex-row items-stretch   ">
-            <div class="py-4 box-border h-1/2 w-1/2  p-4 ">
-            <p class="text-center font-bold text-blue-700">MODIFICA PROFILO </p>
-             <div class="self-center ">
-                   <i class="fas fa-user-circle h-30"></i>
+        <div class="flex flex-row items-stretch align-middle py-2  ">
+            <div class="py-4 box-border h-1/2 w-1/2  justify-center align-middle">
+            <div class="row flex justify-center align-middle">
+                <p class="text-center font-bold text-blue-700 text-2xl">MODIFICA PROFILO </p>
+            </div>
+             <div class="flex justify-center align-middle  py-6 ">
+                   <img src="../assets/proifile-user-black.svg" class="w-32 h-32">
              </div>
             </div>
 
 
-            <div class="py-4 box-border h-1/2 w-1/2 p-4 ">
+            <div class="py-4 box-border h-1/2 w-1/2 py-2 ">
                 <div class=" md:gap-2"> 
                 <input type="text" name="mail" class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm" placeholder="Nome"> 
                 <input type="text" name="mail" class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm" placeholder="Cognome">
@@ -30,7 +32,7 @@
        
            
         </div>
-         <div class="object-center"> 
+         <div class="flex justify-center"> 
              <button class="bg-blue-800 outline-none text-white font-bold py-2 px-4 rounded pl-1/2"><span>Salva modifiche</span></button>
          </div>
              
