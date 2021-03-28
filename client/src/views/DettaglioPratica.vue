aaee50275d4c4d8032f616b0bf8031c8fcc82973
Simone <galiottosimone@gmail.com>
2021-03-28 15:00:57 +0200

    add v-model in DettaglioPratica

--- client/src/views/DettaglioPratica.vue
+++ client/src/views/DettaglioPratica.vue
@@ -8,75 +8,85 @@
       </div>
       <div class="container mx-auto px-10">
         <div class="grid md:grid-cols-2 md:gap-2">
-          <span
+          <input
             type="text"
             name="company"
-            
+            v-model="form.company.id"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
-            value="Ditta"
+            placeholder="Ditta"
+            disabled
           />
-          <span
+          <input
             type="text"
             name="cee_code"
-            
+            v-model="form.company.cee_code"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Codice C.E."
+            disabled
           />
           <input
             type="text"
             name="full_name"
-            
+            v-model="form.company.full_name"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Al Signor"
+            disabled
           />
           <input
             type="text"
             name="birth_date"
-            
+            v-model="form.company.birth_date"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Nato il"
+            disabled
           />
           <input
             type="text"
             name="residence"
-            
+            v-model="form.company.residence"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Residente a"
+            disabled
           />
           <input
             type="text"
             name="province"
-            
+            v-model="form.company.province"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Prov."
+            disabled
           />
           <input
             type="text"
             name="cap"
-            
+            v-model="form.company.cap"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Cap"
+            disabled
           />
           <input
             type="text"
             name="position"
-            
+            v-model="form.company.position"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Qualifica"
+            disabled
           />
           <input
             type="text"
             name="salary"
-            
+            v-model="form.company.salary"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Paga oraria"
+            disabled
           />
           <input
             type="text"
             name="hire_date"
-            
+            v-model="form.company.hire_date"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Data assunzione"
+            disabled
           />
         </div>
         <div class="flex">
@@ -89,8 +99,10 @@
                 
                 type="radio"
                 class="form-radio"
+                v-model="form.extraprofessional_incident"
                 name="extraprofessional_incident"
                 value="1"
+                disabled
               />
               <span class="ml-2">Si</span>
             </label>
@@ -99,8 +111,10 @@
                 type="radio"
                 
                 class="form-radio"
+                v-model="form.extraprofessional_incident"
                 name="extraprofessional_incident"
                 value="0"
+                disabled
               />
               <span class="ml-2">No</span>
             </label>
@@ -112,8 +126,10 @@
                 
                 type="radio"
                 class="form-radio"
+                v-model="form.car_crash"
                 name="car_crash"
                 value="1"
+                disabled
               />
               <span class="ml-2">Si</span>
             </label>
@@ -122,14 +138,17 @@
                 type="radio"
                
                 class="form-radio"
+                v-model="form.car_crash"
                 name="accountType"
                 value="0"
+                disabled
               />
               <span class="ml-2">No</span>
             </label>
           </div>
         </div>
         <div class="grid md:grid-cols-2 md:gap-2">
+          <!--
           <input
             type="text"
             name="trimester"
@@ -137,62 +156,82 @@
             class="border rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Trimestre di riferimento"
           />
+          -->
+          <select
+            class="border rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
+            v-model="form.trimester"
+            disabled
+          >
+            <option :value="null" disabled>Trimestre di riferimento </option>
+            <option :value="1">Primo trimestre </option>
+            <option :value="2">Secondo trimestre </option>
+            <option :value="3">Terzo trimestre </option>
+            <option :value="4">Quarto trimestre </option>
+          </select>
           <input
             type="text"
             name="weekly_working_hours"
-            
+            v-model="form.weekly_working_hours"
             class="border rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Ore settimanali di lavoro"
+            disabled
           />
           <input
             type="text"
             name="ordinary_work_hours"
-          
+            v-model="form.ordinary_work_hours"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Ore ordinarie di lavoro"
+            disabled
           />
           <input
             type="text"
             name="disease_hours"
-           
+            v-model="form.disease_hours"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Ore di malattia/infortunio"
+            disabled
           />
           <input
             type="text"
             name="cig_hours"
-           
+            v-model="form.cig_hours"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Ore di C.I.G."
+            disabled
           />
           <input
             type="text"
             name="holidays_and_remunerated_permissions_hours"
-            
+            v-model="form.holidays_and_remunerated_permissions_hours"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Ore per ferie e permessi retribuiti"
+            disabled
           />
           <input
             type="text"
             name="other_remunerated_hours"
-            
+            v-model="form.other_remunerated_hours"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Altre retribuite"
+            disabled
           />
           <input
             type="text"
             name="total_hours"
-          
+            v-model="form.total_hours"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Totale ore trimestre"
+            disabled
           />
         </div>
         <input
           type="text"
           name="holidays_and_remunerated_permissions_hours"
-        
+          v-model="form.holidays_and_remunerated_permissions_hours"
           class="border rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
           placeholder="Orario settimanale applicato nel periodo denunciato: ore"
+          disabled
         />
 
         <h3 class="text-lg font-bold py-2">Documenti da allegare:</h3>
@@ -224,16 +263,18 @@
           <input
             type="text"
             name="full_name"
-           
+            v-model="form.company.full_name"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Signor"
+            disabled
           />
           <input
             type="text"
             name="birth_date"
-           
+            v-model="form.company.birth_date"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Nato il"
+            disabled
           />
         </div>
         <h3 class="text-lg font-bold mt-6">Periodo denunciato:</h3>
@@ -243,23 +284,26 @@
           <input
             type="text"
             name="disease_start_date"
-           
+            v-model="form.disease_start_date"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="Da"
+            disabled
           />
           <input
             type="text"
             name="disease_end_date"
-         
+            v-model="form.disease_end_date"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="A"
+            disabled
           />
           <input
             type="text"
             name="total_disease_days"
-       
+            v-model="form.total_disease_days"
             class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
             placeholder="= Giorni"
+            disabled
           />
         </div>
 
@@ -279,24 +323,30 @@
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_not_cleared_1"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_cleared_1"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.daily_amount_1"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
             </tr>
@@ -306,24 +356,30 @@
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_not_cleared_2"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_cleared_2"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.daily_amount_2"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
             </tr>
@@ -333,24 +389,30 @@
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_not_cleared_3"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_cleared_3"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.daily_amount_3"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
             </tr>
@@ -360,24 +422,30 @@
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_not_cleared_4"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_cleared_4"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.daily_amount_4"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
             </tr>
@@ -387,24 +455,30 @@
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_not_cleared_5"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_cleared_5"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.daily_amount_5"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
             </tr>
@@ -417,24 +491,30 @@
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_not_cleared_6"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.days_cleared_6"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
               <td>
                 <input
                   type="text"
                   name="mail"
+                  v-model="form.daily_amount_6"
                   class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
                   placeholder=""
+                  disabled
                 />
               </td>
             </tr>
@@ -448,8 +528,10 @@
             <input
               type="text"
               name="mail"
+              v-model="form.total_final"
               class="border shadow-sm rounded h-10 w-full focus:outline-none focus:border-blue-800 px-2 mt-2 text-sm"
               placeholder="Da"
+              disabled
             />
           </div>
         </div>
